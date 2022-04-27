56117be5b659ab63dee9bf5b062635ec18069673
dorcasndungu <ndungumwihaki017@gmail.com>
2022-04-27 09:31:23 +0300

    this adds the globe icon to the text

--- app/src/main/java/com/example/earth/ui/LaunchActivity.java
+++ app/src/main/java/com/example/earth/ui/LaunchActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.earth.R;
+
 public class LaunchActivity extends AppCompatActivity {
 
     @Override
