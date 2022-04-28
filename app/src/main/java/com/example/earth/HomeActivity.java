9b192c01489f64787e8798b5dfb4f7def250f4db
priscahlimo <priscahlimoh@gmail.com>
2022-04-28 21:57:07 +0300

    refactor stories display

--- app/src/main/java/com/example/earth/HomeActivity.java
+++ app/src/main/java/com/example/earth/HomeActivity.java
@@ -1,14 +1,21 @@
 package com.example.earth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private RecyclerView storiesBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    private void init(){
+        storiesBar = findViewById(R.id.storiesrecycler);
     }
 }
